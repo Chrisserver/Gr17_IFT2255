278e8549ff7293c3904ecf21f389f96154f918d2
Chrisserver <66754560+Chrisserver@users.noreply.github.com>
2024-09-18 09:01:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.getElementById("clickMeButton").addEventListener("click", function() {
     alert("You clicked the button!");
 });
+document.getElementById("Log in").addEventListener("click", function() {
+    <form action="template.html" method="Post">
+        Username: <input type="text" name="Username" /><br />
+        Password: <input type="password" name="Password" /><br />
+        <input type="submit" value="login!" />
+    </form>
+});
